77c28fbf45cdba00b5272bc93f62fe7627bdd930
Sophie Helf <sophie.helf@gmail.com>
2016-06-04 00:07:30 -0700

    First

---
+++ js/summonthespirits.js
@@ -0,0 +1,24 @@
+function GetValue()
+{
+    var myarray= new Array("Stick to! I do hope you have a very good day of the week.",
+        "These are really wonderful ideas in regarding blogging",
+        "miscarriage",
+        "Was chatting about this at Outback Steakhouse. Don't remember how it came up.",
+        "These wow gold are seriously surprise and comfy!! :)",
+        "In relation to your knees",
+        "You have be given grant award of $500,000 USD kindly contact. Email",
+        "Read how other people are building a secondary income along side their normal day to day activities.",
+        "Incredibly kudos this specific completely boulders!",
+        "IT'S SHOWTIME",
+        "Pokerbot will allow you to win.",
+        "Hey girl how are things i once was a postman",
+        "But the girls will tend to you. THne the screen l it up again. Dout me iff u will,",
+        "Paper region egg effec govern",
+        "if you give me. i think your heard is so big. i think you love god,",
+        "PLESE CONTACT ME ON MY PRIVATE EMAIL",
+        "How are you doing my dear? Hope fine.",
+        "Carpenter? Plumber? painter?",
+        "always with me, tiny dancer");
+    var random = myarray[Math.floor(Math.random() * myarray.length)];
+   document.getElementById("message").innerHTML=random;
+}
